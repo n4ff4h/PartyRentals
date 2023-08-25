147e135fc6b335a377362e31be464fdb5b0dfa61
Naffah Abdulla Rasheed <mail@naffah.me>
2023-08-25 18:50:31 +0500

    refactor: Create Product model

---
+++ src/main/java/me/naffah/partyrentals/models/Product.java
@@ -0,0 +1,98 @@
+package me.naffah.partyrentals.models;
+
+import java.sql.Date;
+
+public class Product {
+    private int id;
+    private String name;
+    private String sku;
+    private String description;
+    private int qty;
+    private double price;
+    private int categoryId;
+    private Date createdDate;
+    private Date modifiedDate;
+
+    public Product(String name, String sku, String description, int qty, double price, int categoryId) {
+        this.name = name;
+        this.sku = sku;
+        this.description = description;
+        this.qty = qty;
+        this.price = price;
+        this.categoryId = categoryId;
+    }
+
+    public Product() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getModifiedDate() {
+        return modifiedDate;
+    }
+
+    public void setModifiedDate(Date modifiedDate) {
+        this.modifiedDate = modifiedDate;
+    }
+}
